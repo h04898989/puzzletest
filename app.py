d7a15f5d300bc219e50500d08ae19d7c62873e49
U-IEC\IEC111491 <IEC111491@TAO-111491-1.iec.inventec>
2025-04-27 13:05:10 +0800

    sara update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # 讀取遊戲名稱資料
 with open('games.json', 'r', encoding='utf-8') as f:
-    GAME_NAMES = json.load(f)
+    GAME_DATA = json.load(f)
 
 @app.route('/')
 def home():
@@ -13,13 +13,24 @@ def home():
 
 @app.route('/game/<game_id>')
 def game(game_id):
-    game_name = GAME_NAMES.get(game_id, "未知遊戲")  # 根據 game_id 獲取遊戲名稱
+    game_name = GAME_DATA.get(game_id, {}).get("name", "未知遊戲")
     return render_template('levels.html', game_id=game_id, game_name=game_name)
 
 @app.route('/game/<game_id>/level/<level_id>')
 def level(game_id, level_id):
-    game_name = GAME_NAMES.get(game_id, "未知遊戲")  # 根據 game_id 獲取遊戲名稱
-    return render_template('index.html', game_id=game_id, level_id=level_id, game_name=game_name)
+    game = GAME_DATA.get(game_id, {})
+    game_name = game.get("name", "未知遊戲")
+    level_data = game.get("levels", {}).get(level_id, {})
+    description = level_data.get("description", "沒有劇情描述")
+    hint = level_data.get("hint", "沒有提示")
+    return render_template(
+        'index.html',
+        game_id=game_id,
+        level_id=level_id,
+        game_name=game_name,
+        description=description,
+        hint=hint
+    )
 
 @app.route('/validate', methods=['POST'])
 def validate():
