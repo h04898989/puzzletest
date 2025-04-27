89942b0cc757c265eee18e496ec2baf2ecbc53fb
U-IEC\IEC111491 <IEC111491@TAO-111491-1.iec.inventec>
2025-04-27 15:15:48 +0800

    add button margin

--- push.sh
+++ push.sh
@@ -1,3 +1,13 @@
-git add .
-git commit -m "sara update"
-git push origin
+#!/bin/bash
+
+# 檢查是否提供參數作為 commit 訊息
+if [ -z "$1" ]; then
+  commit_message="sara update" # 預設 commit 訊息
+  else
+    commit_message="$1" # 使用提供的參數作為 commit 訊息
+    fi
+
+    # 執行 Git 指令
+    git add .
+    git commit -m "$commit_message"
+    git push origin
